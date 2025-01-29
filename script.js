35aa01fb877797860e4994dec3280b2327049245
GIRIDHANU796 <727723euec050@skcet.ac.in>
2025-01-29 09:35:28 +0530

    1st

---
+++ script.js
@@ -0,0 +1,14 @@
+function handleLogin()
+{
+    const username=document.getElementById('user').value;
+    const password=document.getElementById('pass').value;
+    const error=document.getElementById('error');
+
+}
+const loginData=[
+    {username:"cristiano",password:"pass123"},
+    {username:"virat",password:"123pass"},
+    {username:"cristiano",password:"pass123"},
+]
+
+
